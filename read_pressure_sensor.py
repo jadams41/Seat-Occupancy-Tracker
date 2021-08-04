0f4c815925e63cf3c87b59c28c8810474b215cf8
Ethan Adams <jadamsm@amazon.com>
2021-08-04 20:08:13 +0200

    update read pressure sensor

--- read_pressure_sensor.py
+++ read_pressure_sensor.py
@@ -11,12 +11,13 @@ try:
     while True:
         # take a reading
         input = GPIO.input(4)
+        print(f"input = {input}")
         if ((not prev_input) and input):
             print("Under pressure")
         #update previous input
         prev_input = input
         #slight pause
-        time.sleep(0.10)
+        time.sleep(1)
 except KeyboardInterrupt:
     print("Stopping the program")
     pass
