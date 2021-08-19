0cec1342a8b2d99058c109a614bd52c41bbcd5ce
Ethan Adams <jadamsm@amazon.com>
2021-08-19 21:38:10 +0200

    client/server model expounded

--- web_server.py
+++ web_server.py
@@ -1,27 +1,25 @@
 # Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-from datetime import datetime
 import json
 import random
-# import RPi.GPIO as GPIO
-from gpiozero import InputDevice
-
+import socket
+import time
+from datetime import datetime
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 hostName = "localhost"
 serverPort = 9009
-input_device=InputDevice(17)
 
-def get_current_time_string():
-        now = datetime.now().time()
-        return f"The current time is: {now}"
+current_seats = {
+        'Dummy Seat #1': "Dummy value",
+        'Dummy Seat #2': "Dummy value"
+}
 
-def seat_currently_occupied():
-        # current_value = GPIO.input(26)
-        current_value=input_device.value
-        print(f'pin 17 reads: {current_value}')
-        return current_value == 1
-        # return random.randint(0,1) == 0
+def process_received_data(data):
+        print(f"Recieved data!: {data}")
+        if not data['client_id'] or not data['seat_occupied']:
+                print("Data was invalid... ignoring")
+
+        current_seats[data['client_id']] = data['seat_occupied']
 
 class MyServer(BaseHTTPRequestHandler):
         def do_GET(self):
@@ -33,7 +31,6 @@ class MyServer(BaseHTTPRequestHandler):
                         self.end_headers()
                         with open('index.html', 'r') as webpage_file:
                                 self.wfile.write(bytes(webpage_file.read(), "utf-8"))
-                                # self.wfile.write(bytes(get_current_pressure_reading(), 'utf-8'))
 
                 elif self.path == '/get_current_occupancy':
                         print("Returning current occupancy readings")
@@ -42,23 +39,14 @@ class MyServer(BaseHTTPRequestHandler):
                         self.send_header("Content-type", "application/json")
                         self.end_headers()
 
-                        current_value = [
-                                {
-                                        "seatName": "Seat #1",
-                                        "current_occupied": seat_currently_occupied()
-                                },
-                                {
-                                        "seatName": "Seat #2",
-                                        # "current_occupied": True
-                                },
-                                {
-                                        "seatName": "Seat #3",
-                                        # "current_occupied": True
-                                },
-                                {
-                                        "seatName": "Seat #4", # "current_occupied": True
-                                }
-                        ]
+                        current_value = []
+
+                        for key in current_seats:
+                                current_value.append({
+                                        'seatName': key,
+                                        'current_occupied':current_seats[key]
+                                })
+
                         print(f"Returning current readings: {json.dumps(current_value)}")
                         self.wfile.write(bytes(f'{json.dumps(current_value)}', 'utf-8'))
                 else:
@@ -68,17 +56,29 @@ class MyServer(BaseHTTPRequestHandler):
                 print("Someone posted to me!")
                 if self.path == '/upload_sensor_data':
                         print("Received post")
-                        print(self.data)
 
-                print(self)
+                        content_length = int(self.headers['Content-Length'])
+                        post_data = self.rfile.read(content_length)
+                        received_data = json.loads(post_data)
+                        process_received_data(received_data)
+
                 self.send_response(200)
                 self.send_header("Content-type", "application/json")
                 self.end_headers()
 
-
 if __name__ == "__main__":
         webServer = HTTPServer((hostName, serverPort), MyServer)
+
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+
         print("Server started http://%s:%s" % (hostName, serverPort))
+        print(f'Things are running on hostname={hostname}, local_ip={local_ip}')
+        try:
+                local_ip2=([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])[0]
+                print(f"IP Address is: {local_ip2}")
+        except:
+                pass
 
         try:
                 webServer.serve_forever()
